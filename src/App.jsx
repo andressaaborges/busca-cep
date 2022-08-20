009bcd4159f6fbb8d4dd13ae5ac24cb3d24328c5
andressaaborges <emylleaborges@gmail.com>
2022-08-20 12:17:38 -0300

    remove unnecessary codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
 
       </div>
-      )
+  )
 }
 
 export default App
